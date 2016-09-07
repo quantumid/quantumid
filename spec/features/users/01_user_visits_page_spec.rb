36c052f040fbc5e86229fb333a7041e38fde66c5
Jimmy Ranger <jranger@me.com>
2016-09-07 19:33:15 -0400

    adds early feature tests

---
+++ spec/features/users/01_user_visits_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'video on home page', %{
+  As an unauthenticated user
+  I want to visit the home page
+  So I can view content
+} do
+
+  context 'before signing in' do
+
+    scenario 'user visits page' do
+      expect(:page).to have_css('video')
+      expect(:page).to have_content('QUANTUMi.d.')
+    end
+
+    scenario 'user can sign in through facebook' do
+      expect(:page).to have_link('Sign Up through Facebook')
+      expect(:page).to have_link('Sign In')
+    end
+
+  end
+
+end
