2055ba677a4aecd04898ecc981dfce2b89994ae8
Jimmy Ranger <jranger@me.com>
2016-09-10 13:49:37 -0400

    factories for votes, videos, channels

--- spec/factories/channels.rb
+++ spec/factories/channels.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :channel do
-    
+      name 'Nickelodeon'
+      description 'Nick-Nick-Na-Nick, Na-Nick-Nick-Nick, Nickelodeon'
+      vimeo_id 1129301
   end
 end
--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :video do
-    
+    title 'Doug'
+    description 'Dear Journal...'
+    vimeo_id 150707411
+    channel
   end
 end
