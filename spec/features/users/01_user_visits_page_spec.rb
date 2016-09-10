ce6ccfaeeac3c49201ea0f8d80c36814ee407452
Jimmy Ranger <jranger@me.com>
2016-09-10 12:06:47 -0400

    moves to new repo

--- spec/features/users/01_user_visits_page_spec.rb
+++ spec/features/users/01_user_visits_page_spec.rb
@@ -12,6 +12,32 @@ feature 'video on home page', %(
     scenario 'user can sign in through facebook' do
       expect(:page).to have_link('Sign Up through Facebook')
       expect(:page).to have_link('Sign In')
+      expect(:page).to_not have_link('Sign Out')
+    end
+    scenario 'user can see vote buttons' do
+      expect(:page).to have_button 'Upvote'
+      expect(:page).to have_button 'Downvote'
+    end
+    scenario 'user cannot use vote buttons' do
+      click_button 'Upvote'
+      expect(:page).to have_content 'You must sign in before voting!'
+
+      click_button 'Downvote'
+      expect(:page).to have_content 'You must sign in before voting!'
+    end
+  end
+
+  context 'after signing in' do
+    scenario 'user can sign out, but not sign up' do
+      expect(:page).to_not have_link('Sign Up')
+      expect(:page).to_not have_link('Sign Up through Facebook')
+      expect(:page).to have_link('Sign Out')
+    end
+    scenario 'user can use upvote buttons' do
+      click_button 'Upvote'
+
+      expect(:page).to have_content 'Upvoted!'
+      expect(:page).to_not have_button 'Upvote'
     end
   end
 end
