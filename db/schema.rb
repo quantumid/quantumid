5465dc482a6122e5bb7e634ae05b3265c3cfd30a
Jimmy Ranger <jranger@me.com>
2016-09-10 13:23:38 -0400

    adds votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910170320) do
+ActiveRecord::Schema.define(version: 20160910171846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20160910170320) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["channel_id"], name: "index_videos_on_channel_id", using: :btree
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "video_id"
+    t.boolean  "upvote",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["video_id"], name: "index_votes_on_video_id", using: :btree
   end
 
 end
