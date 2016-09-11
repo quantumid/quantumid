20821a69e286f4041954726a41ad286ce9a5a258
Jimmy Ranger <jranger@me.com>
2016-09-11 16:56:49 -0400

    a little refactoring

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,14 +1,14 @@
 class ChannelsController < ApplicationController
-  
+
   def index
     @all_channels = Channel.all
+    @initial_video = nil
     first_channel = @all_channels.first
 
     if first_channel
       @initial_video = first_channel.videos.first.vimeo_id
-    else
-      @initial_video = nil
     end
+    
   end
 
 end
