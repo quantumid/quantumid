2e8244648cffd5a1abe0025d41def1d8fcbcf28e
Jimmy Ranger <jranger@me.com>
2016-09-11 16:23:52 -0400

    hound indentation error fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,13 +40,13 @@ RSpec.configure do |config|
   config.before(:suite) do
    DatabaseCleaner.strategy = :transaction
    DatabaseCleaner.clean_with(:truncation)
- end
+  end
 
- config.around(:each) do |example|
+  config.around(:each) do |example|
    DatabaseCleaner.cleaning do
      example.run
    end
- end
+  end
 
   config.infer_spec_type_from_file_location!
 
