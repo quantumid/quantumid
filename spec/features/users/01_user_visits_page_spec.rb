74a4b445880b587014227b32f1c7590a8473d3dd
Jimmy Ranger <jranger@me.com>
2016-09-11 17:01:24 -0400

    makes original tests pending

--- spec/features/users/01_user_visits_page_spec.rb
+++ spec/features/users/01_user_visits_page_spec.rb
@@ -6,19 +6,23 @@ feature 'video on home page', %(
 ) do
   context 'before signing in' do
     scenario 'user visits page' do
+      pending
       expect(:page).to have_css('video')
       expect(:page).to have_content('QUANTUMi.d.')
     end
     scenario 'user can sign in through facebook' do
+      pending
       expect(:page).to have_link('Sign Up through Facebook')
       expect(:page).to have_link('Sign In')
       expect(:page).to_not have_link('Sign Out')
     end
     scenario 'user can see vote buttons' do
+      pending
       expect(:page).to have_button 'Upvote'
       expect(:page).to have_button 'Downvote'
     end
     scenario 'user cannot use vote buttons' do
+      pending
       click_button 'Upvote'
       expect(:page).to have_content 'You must sign in before voting!'
 
@@ -29,11 +33,13 @@ feature 'video on home page', %(
 
   context 'after signing in' do
     scenario 'user can sign out, but not sign up' do
+      pending
       expect(:page).to_not have_link('Sign Up')
       expect(:page).to_not have_link('Sign Up through Facebook')
       expect(:page).to have_link('Sign Out')
     end
     scenario 'user can use upvote buttons' do
+      pending
       click_button 'Upvote'
 
       expect(:page).to have_content 'Upvoted!'
