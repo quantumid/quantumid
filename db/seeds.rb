e0f76e5240b5d83c88ca87fa9a0d6417b9398fbe
Jimmy Ranger <jranger@me.com>
2016-09-16 17:53:19 -0400

    hound violations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 if Channel.where(name: 'Super Fun Sample Test Channel').blank?
-  Channel.create!(name: 'Super Fun Sample Test Channel', vimeo_id: 1129301)
+  Channel.create!(name: 'Super Fun Sample Test Channel', vimeo_id: 112_930_1)
 end
 
 if Video.where(title: 'Nickelodeon\'s Doug (opening credits)').blank?
   cid = Channel.where(vimeo_id: 1129301).first.id
-  Video.create!(vimeo_id: 150707411, title: 'Nickelodeon\'s Doug (opening credits)', channel_id: cid)
+  Video.create!(vimeo_id: 150_707_411, title: 'Nickelodeon\'s Doug (opening credits)', channel_id: cid)
 end
