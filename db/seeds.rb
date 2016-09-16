7109e55b8cfe10962a6e3fd78cb023de391ea288
Jimmy Ranger <jranger@me.com>
2016-09-16 17:50:38 -0400

    adds seeds file so we're all on the same page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Channel.where(name: 'Super Fun Sample Test Channel').blank?
+  Channel.create!(name: 'Super Fun Sample Test Channel', vimeo_id: 1129301)
+end
+
+if Video.where(title: 'Nickelodeon\'s Doug (opening credits)').blank?
+  cid = Channel.where(vimeo_id: 1129301).first.id
+  Video.create!(vimeo_id: 150707411, title: 'Nickelodeon\'s Doug (opening credits)', channel_id: cid)
+end
