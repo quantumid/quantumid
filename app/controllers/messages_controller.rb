17d4908fefbaba33cc1ebdf065a0c63f8c502bcc
Jimmy Ranger <jranger@me.com>
2016-09-22 14:11:00 -0400

    AC tests

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+  before_action :require_current_user
+
+  def index
+    @current_messages = Message.current.includes(:user).reverse
+  end
+
+  def create
+    message = current_user.messages.create(message_params)
+    ActionCable.server.broadcast 'messages', { message: message,
+                                               user: current_user }
+    head :ok
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
