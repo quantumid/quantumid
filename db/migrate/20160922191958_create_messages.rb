8ebc67907a5e2a5ae7cdf459172c8a2ce7b3c854
Jimmy Ranger <jranger@me.com>
2016-09-22 15:31:38 -0400

    adds messages and chats to db

--- db/migrate/20160922191958_create_messages.rb
+++ db/migrate/20160922191958_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.string :content, null: false
+      t.belongs_to :chat
+      t.belongs_to :user
       t.timestamps
     end
   end
--- db/migrate/20160922192035_create_chats.rb
+++ db/migrate/20160922192035_create_chats.rb
@@ -1,7 +1,8 @@
 class CreateChats < ActiveRecord::Migration[5.0]
   def change
     create_table :chats do |t|
-
+      t.string :name, null: false
+      t.string :topic
       t.timestamps
     end
   end
