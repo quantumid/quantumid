8149affa88ac2bc7f6e3e1aa0ee0cc1d881dc57d
Jimmy Ranger <jranger@me.com>
2016-09-23 15:42:17 -0400

    changes param key

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,21 @@
 class MessagesController < ApplicationController
+  def index
+    @chat = Chat.find(params[:chat_id])
+    @messages = @chat.messages.first(20)
+  end
+
   def show
-    @chat = Chat.find(params[:id])
-    @messages = chat.messages.first(20)
+    @chat = Chat.find(params[:chat_id])
+    @message = Message.find(params[:message_id])
   end
 
   def new
-    Message.new
+    @chat = Chat.find(params[:chat_id])
+    @message = Message.new
   end
 
   def create
-    @chat = Chat.find(params[:id])
+    @chat = Chat.find(params[:chat_id])
     @message = Message.new(new_message_params)
     @message.user = current_user
     redirect_to @chat
