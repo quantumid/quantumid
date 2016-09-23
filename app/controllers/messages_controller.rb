da1433c0db58c3d3e4df541c77e3de178a4ca3fc
Jimmy Ranger <jranger@me.com>
2016-09-23 12:38:25 -0400

    adds message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    chat = Chat.find(params[:chat])
+    @messages = chat.messages.first(20)
   end
+
+  def new
+    Message.new
+  end
+
+  def create
+    message = Message.new(new_message_params)
+    message.user = current_user
+  end
+
+  private
+
+  def new_message_params
+    params.require(:message).permit(:content)
+  end
+
 end
