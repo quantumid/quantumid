2db3fcdd20400d3b2ba4b85dd8ce4771cb257201
Jimmy Ranger <jranger@me.com>
2016-09-26 12:40:41 -0400

    adds fb ouauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926162015) do
+ActiveRecord::Schema.define(version: 20160926163357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 20160926162015) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username",                            null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "name",                                null: false
