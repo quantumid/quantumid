f6963fe2e91126c1bbbeaaf286deea16cd464f28
Kristin Ruben <kruben@gmail.com>
2016-09-30 15:00:10 -0400

    working on visuals

--- db/schema.rb
+++ db/schema.rb
@@ -23,23 +23,6 @@ ActiveRecord::Schema.define(version: 20160926163357) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "chats", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "topic"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string   "content",    null: false
-    t.integer  "chat_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chat_id"], name: "index_messages_on_chat_id", using: :btree
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
