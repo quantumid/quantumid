7069afab126fa262237c5947f95bf258965604c1
Jimmy Ranger <jranger@me.com>
2016-10-12 17:22:13 -0400

    modifies messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,28 +1,3 @@
 class MessagesController < ApplicationController
-  def index
-    @chat = Chat.find(params[:chat_id])
-    @messages = @chat.messages
-  end
-
-  def new
-    @chat = Chat.find(params[:chat_id])
-    @message = Message.new
-  end
-
-  def create
-    @message = Message.new(new_message_params)
-    @message.user = current_user
-    @message.chat = Chat.find(params[:chat_id])
-    if @message.save
-      redirect_to chat_messages_path(@message.chat)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def new_message_params
-    params.require(:message).permit(:content)
-  end
+  after_create_commit { MessageBroadcastJob.perform_later self }
 end
